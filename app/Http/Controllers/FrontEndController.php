a7fc9dacf7887135ca5efcf5595baa9f18ecbb98
MD. Mahidul Islam <32381876+mdmahidulislam@users.noreply.github.com>
2019-02-04 13:48:59 +0600

    dynamically frontend part 3
    dynamically frontend part 3

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -16,6 +16,13 @@ class FrontEndController extends Controller
     	->with('categories', Category::take(10)->get())
     	->with('first_post', Post::orderBy('created_at','desc')->first())
     	->with('second_post', Post::orderBy('created_at','desc')->skip(1)->take(1)->get()->first())
-    	->with('third_post', Post::orderBy('created_at','desc')->skip(2)->take(1)->get()->first());
+    	->with('third_post', Post::orderBy('created_at','desc')->skip(2)->take(1)->get()->first())
+    	->with('africa', Category::find(6))
+    	->with('asia', Category::find(8))
+    	->with('australia', Category::find(9))
+    	->with('europe', Category::find(10))
+    	->with('north_america', Category::find(11))
+    	->with('south_america', Category::find(12))
+    	->with('settings', Setting::first());
     }
 }
