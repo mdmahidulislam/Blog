663e14a9162b81c262b681b361374347fd9d13b6
MD. Mahidul Islam <32381876+mdmahidulislam@users.noreply.github.com>
2019-02-08 21:04:18 +0600

    endning

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,33 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-
             <div class="col-lg-3">
                 <div class="panel panel-info">
                     <div class="panel-heading text-center">
-                        POSTED
+                         POSTED
                     </div>
                     <div class="panel-body">
-                        <h1 class="text-center">
-                            {{ $posts_count}}
-                        </h1>
-                        
+                        <h1 class="text-center">{{ $posts_count }}</h1>
                     </div>
                 </div>
             </div>
 
-
-
-             <div class="col-lg-3">
+            <div class="col-lg-3">
                 <div class="panel panel-danger">
                     <div class="panel-heading text-center">
                         TRASHED POSTS
                     </div>
                     <div class="panel-body">
-                        <h1 class="text-center">
-                            {{ $trashed_count}}
-                        </h1>
-                        
+                        <h1 class="text-center">{{ $trashed_count }}</h1>
                     </div>
                 </div>
             </div>
@@ -35,29 +26,22 @@
             <div class="col-lg-3">
                 <div class="panel panel-success">
                     <div class="panel-heading text-center">
-                        USERS
+                        USERS 
                     </div>
                     <div class="panel-body">
-                        <h1 class="text-center">
-                           {{$users_count}} 
-                        </h1>
-                        
+                        <h1 class="text-center">{{ $users_count }}</h1>
                     </div>
                 </div>
             </div>
-            
-                <div class="col-lg-3">
-                    <div class="panel panel-info">
-                        <div class="panel-heading text-center">
-                            CATEGORIES
-                        </div>
-                        <div class="panel-body">
-                            <h1 class="text-center">
-                                {{$categories_count}}
-                            </h1>
-                            
-                        </div>
+
+            <div class="col-lg-3">
+                <div class="panel panel-info">
+                    <div class="panel-heading text-center">
+                        CATEGORIES
+                    </div>
+                    <div class="panel-body">
+                        <h1 class="text-center">{{ $categories_count }}</h1>
                     </div>
                 </div>
- 
+            </div>
 @endsection
